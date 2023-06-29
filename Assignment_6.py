35f75dd2e532b898613aa32a0690ea6e7dd87112
Yash Teredesai <107024856+yasht04@users.noreply.github.com>
2023-06-29 15:16:50 +0530

    Add files via upload

---
+++ Assignment_6.py
@@ -0,0 +1,34 @@
+def ds(roll_no,name):
+    n1 = [roll_no,name]
+    n2 = (roll_no,name)
+    n3 = {roll_no,name}
+    n4 = {"roll no":roll_no,"name":name}
+
+    print(n1)
+    print(n2)
+    print(n3)
+    print(n4)
+
+    roll = int(input("Enter Roll no: "))
+    name = input("Enter name: ")
+
+    n1[0] = roll
+    n1[1] = name
+    n2 = (roll,name)
+    n3 = {roll,name}
+    n4["roll no"]= roll
+    n4["name"]=name
+
+    print("list:",n1)
+    print("tuple:",n2)
+    print("set:",n3)
+    print("dictionary:",n4)
+
+    del n1
+    del n2
+    del n3
+    del n4
+
+roll = int(input("enter roll no.: "))
+name = input("Enter name: ")
+ds(roll,name)
