aa4eba1b79d2f8a71bbec866805b2d2e79a62741
Yash Teredesai <107024856+yasht04@users.noreply.github.com>
2023-07-01 21:35:10 +0530

    Updated Assignment 7.py

--- Assignment 7.py
+++ Assignment 7.py
@@ -1,11 +1,16 @@
 def func(file):
-    f = open(file,'+a')
-    roll = int(input("Enter Your Roll No.: "))
-    name = input("Enter Your Name: ")
-    class_ = input("Enter Your Class: ")
-    f.writelines([f"Roll No.: {roll},",f" Name: {name},",f" Class: {class_}"])
-    f.seek(0)
-    print(f.readlines())
+    try:
+        f = open(file,'+a')
+        roll = int(input("Enter Your Roll No.: "))
+        name = input("Enter Your Name: ")
+        class_ = input("Enter Your Class: ")
+        f.writelines([f"Roll No.: {roll},",f" Name: {name},",f" Class: {class_}"])
+        f.seek(0)
+        print(f.readlines())
+    except Exception:
+        print("Error!")
+    finally:
+        f.close()
 
 file = input("Enter file name and add .txt at end: ")
 func(file)
