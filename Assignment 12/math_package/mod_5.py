f9c8418c9739e3de529b31272dff03988e55718d
Yash Teredesai <107024856+yasht04@users.noreply.github.com>
2023-07-06 21:34:04 +0530

    Delete Assignment 12 directory

--- Assignment 12/math_package/mod_5.py
+++
@@ -1,9 +0,0 @@
-def para():
-    b = float(input("Enter side of parallelogram: "))
-    h = float(input("Enter height of parallelogram: "))
-    a = b*h
-    print("area of parallelogram:",a)
-    p = 2*(b+h)
-    print("perimeter of parallelogram:",p)
-    with open("save_data.txt","+a") as f:
-        f.writelines([f"\nParallelogram:\nSide:{b}\nHeight:{h}\nArea:{a}\nPerimeter:{p}"])
