268fa716bb03a1b7db1448d77b0633351409c714
Yash Teredesai <107024856+yasht04@users.noreply.github.com>
2023-08-05 20:26:35 +0530

    Update mod_2.py

--- Assignment_12/math_package/mod_2.py
+++ Assignment_12/math_package/mod_2.py
@@ -1,16 +1,16 @@
-class input(Exception):
+class inp(Exception):
     def __init__(self,*i):
         super().__init__(*i)
 def square():
     try:
         a = float(input("Enter length of side of square: "))
         if a<=0:
-            raise input("cannot accept 0 as length.")
+            raise inp("cannot accept 0 as length.")
         c = 4*a
         print("Circumference:",c)
         s = a*a
         print("Area:",s)
         with open("save_data.txt","+a") as f:
             f.writelines([f"\nSquare:\nSide length:{a}\nCircumference:{c}\nArea:{s}"])
-    except input as i:
+    except inp as i:
         print(i.args[0])
