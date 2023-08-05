35e324b98d09e5276cb7d1c8d99fc123873e12d2
Yash Teredesai <107024856+yasht04@users.noreply.github.com>
2023-08-05 20:25:35 +0530

    Update mod_1.py

--- Assignment_12/math_package/mod_1.py
+++ Assignment_12/math_package/mod_1.py
@@ -1,17 +1,17 @@
 import math as m
-class input(Exception):
+class inp(Exception):
     def __init__(self,*i):
         super().__init__(*i)
 def circle():
     try:
         r = float(input("Enter Radius of Circle: "))
         if r<=0:
-            raise input("cannot accept 0 as Radius.")
+            raise inp("cannot accept 0 as Radius.")
         a = m.pi*m.pow(r,2)
         print("Area of circle:",a)
         cir = 2*m.pi*r
         print("Circumference:",cir)
         with open("save_data.txt","+a") as f:
             f.writelines([f"\ncircle:\nRadius:{r}\nArea:{a}\nCircumference:{cir}"])
-    except input as p:
+    except inp as p:
         print(p.args[0])
